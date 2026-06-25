11326ba456d22bec5bbf7dbfe8547c876481a72b
Joel Becerra <18494815-captain-klein@users.noreply.gitlab.com>
2026-06-24 20:43:02 -0600

    feat(infra): add Helm Chart for deploy on my K8s cluster

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -1,98 +1,3 @@
-terraform {
-  cloud {
-    organization = "showoff"
-    workspaces {
-      name = "pet-clinic"
-    }
-  }
-
-  required_providers {
-    argocd = {
-      source  = "argoproj-labs/argocd"
-      version = "7.11.0"
-    }
-  }
-}
-
-provider "aws" {
-  assume_role {
-    role_arn = "arn:aws:iam::${var.deploy_account}:role/TerraformAssumeRole"
-  }
-  default_tags {
-    tags = {
-      IaC = "Terraform"
-    }
-  }
-}
-
-locals {
-  repositories = [
-    "spring-petclinic-admin-server",
-    "spring-petclinic-api-gateway",
-    "spring-petclinic-config-server",
-    "spring-petclinic-customers-service",
-    "spring-petclinic-discovery-server",
-    "spring-petclinic-genai-service",
-    "spring-petclinic-vets-service",
-    "spring-petclinic-visits-service",
-  ]
-}
-
-variable "deploy_account" {
-  type        = string
-  description = "AWS account for deploy resources"
-}
-
-variable "kubernetes_host" {
-  type        = string
-  description = "The API server endpoint (host URL) of the target Kubernetes cluster."
-}
-
-variable "client_certificate" {
-  type        = string
-  description = "The PEM-encoded client certificate used to authenticate to the Kubernetes API server."
-}
-
-variable "client_key" {
-  type        = string
-  description = "The PEM-encoded private key corresponding to the client certificate for Kubernetes authentication."
-}
-
-variable "cluster_ca_certificate" {
-  type        = string
-  description = "The PEM-encoded CA certificate that verifies the Kubernetes API server's certificate."
-}
-
-variable "argocd_url" {
-  type        = string
-  description = "Base URL of the Argo CD instance (e.g., https://argocd.example.com)."
-}
-
-variable "argocd_token" {
-  type        = string
-  description = "API token used by Terraform to authenticate with the Argo CD instance."
-  sensitive   = true
-}
-
-variable "github_pat" {
-  type        = string
-  description = "PAT Github token for get the repository"
-  sensitive   = true
-}
-
-provider "kubernetes" {
-  host                   = var.kubernetes_host
-  client_certificate     = base64decode(var.client_certificate)
-  client_key             = base64decode(var.client_key)
-  cluster_ca_certificate = base64decode(var.cluster_ca_certificate)
-}
-
-provider "argocd" {
-  server_addr = var.argocd_url
-  auth_token  = var.argocd_token
-}
-
-
 resource "argocd_repository" "repository" {
   repo     = "https://github.com/captain-klein/spring-petclinic-microservices.git"
   username = "git"
@@ -112,9 +17,73 @@ resource "aws_ecr_repository" "petclinic" {
   }
 }
 
+resource "aws_ecr_lifecycle_policy" "petclinic" {
+  for_each = aws_ecr_repository.petclinic
+
+  repository = each.value.name
+
+  policy = jsonencode({
+    rules = [
+      {
+        rulePriority = 1
+        description  = "Keep last 20 images"
+        selection = {
+          tagStatus   = "any"
+          countType   = "imageCountMoreThan"
+          countNumber = 20
+        }
+        action = {
+          type = "expire"
+        }
+      }
+    ]
+  })
+}
+
 resource "kubernetes_namespace_v1" "app" {
   metadata {
     name = "pet-clinic"
+  }
+}
+
+resource "vault_generic_secret" "pet_clinic" {
+  path         = "workloads/pet_clinic"
+  disable_read = true
+  data_json = jsonencode(
+    {
+      "foo"   = "bar",
+      "pizza" = "cheese"
+    }
+  )
+  lifecycle {
+    ignore_changes = [
+      data_json,
+    ]
+  }
+}
+
+resource "argocd_application" "deploy" {
+  metadata {
+    name      = "pet-clinic"
+    namespace = "argocd"
+    labels = {
+      "app.kubernetes.io/part-of" = "homelab-apps"
+    }
+  }
+
+  spec {
+    project = "lab-talos-workloads"
+
+    destination {
+      server    = "https://192.168.50.180:6443"
+      namespace = kubernetes_namespace_v1.app.metadata[0].name
+    }
+
+    source {
+      repo_url        = argocd_repository.repository.repo
+      path            = "chart/petclinic"
+      target_revision = "PET-02"
+    }
   }
 }
 
@@ -122,5 +91,118 @@ output "repository_urls" {
   value = {
     for name, repo in aws_ecr_repository.petclinic :
     name => repo.repository_url
+  }
+}
+
+resource "kubernetes_manifest" "pet_clinic" {
+  manifest = {
+    apiVersion = "external-secrets.io/v1"
+    kind       = "ExternalSecret"
+    metadata = {
+      name      = kubernetes_namespace_v1.app.metadata[0].name
+      namespace = kubernetes_namespace_v1.app.metadata[0].name
+    }
+    spec = {
+      secretStoreRef = { name = "vault-dir-workloads", kind = "ClusterSecretStore" }
+      target         = { name = "${kubernetes_namespace_v1.app.metadata[0].name}-env", creationPolicy = "Owner" }
+      data = [
+        {
+          secretKey = "db-host"
+          remoteRef = {
+            key      = "workloads/data/pet_clinic"
+            property = "MYSQL_HOST"
+          }
+        },
+        {
+          secretKey = "db-dbname"
+          remoteRef = {
+            key      = "workloads/data/pet_clinic"
+            property = "MYSQL_DATABASE"
+          }
+        },
+        {
+          secretKey = "db-username"
+          remoteRef = {
+            key      = "workloads/data/pet_clinic"
+            property = "MYSQL_USER"
+          }
+        },
+        {
+          secretKey = "db-password"
+          remoteRef = {
+            key      = "workloads/data/pet_clinic"
+            property = "MYSQL_PASSWORD"
+          }
+        },
+        {
+          secretKey = "SPRING_PROFILES_ACTIVE"
+          remoteRef = {
+            key      = "workloads/data/pet_clinic"
+            property = "SPRING_PROFILES_ACTIVE"
+          }
+        },
+        {
+          secretKey = "SPRING_DATASOURCE_URL"
+          remoteRef = {
+            key      = "workloads/data/pet_clinic"
+            property = "SPRING_DATASOURCE_URL"
+          }
+        },
+        {
+          secretKey = "SPRING_DATASOURCE_USERNAME"
+          remoteRef = {
+            key      = "workloads/data/pet_clinic"
+            property = "MYSQL_USER"
+          }
+        },
+        {
+          secretKey = "SPRING_DATASOURCE_PASSWORD"
+          remoteRef = {
+            key      = "workloads/data/pet_clinic"
+            property = "MYSQL_PASSWORD"
+          }
+        },
+      ]
+    }
+  }
+}
+
+///
+
+data "vault_kv_secret_v2" "petc_clinic" {
+  mount = "workloads"
+  name  = "pet_clinic"
+}
+
+resource "portainer_stack" "db" {
+  name                          = "casa-pet-clinic-db"
+  deployment_type               = "standalone"
+  method                        = "repository"
+  endpoint_id                   = 3
+  repository_url                = "https://github.com/captain-klein/spring-petclinic-microservices.git"
+  repository_reference_name     = "refs/heads/PET-02"
+  file_path_in_repository       = "docker/docker-compose-db.yaml"
+  tlsskip_verify                = false
+  pull_image                    = true
+  git_repository_authentication = false
+
+  env {
+    name  = "MYSQL_ROOT_PASSWORD"
+    value = data.vault_kv_secret_v2.petc_clinic.data["MYSQL_ROOT_PASSWORD"]
+  }
+
+  env {
+    name  = "MYSQL_DATABASE"
+    value = data.vault_kv_secret_v2.petc_clinic.data["MYSQL_DATABASE"]
+  }
+
+  env {
+    name  = "MYSQL_USER"
+    value = data.vault_kv_secret_v2.petc_clinic.data["MYSQL_USER"]
+  }
+
+  env {
+    name  = "MYSQL_PASSWORD"
+    value = data.vault_kv_secret_v2.petc_clinic.data["MYSQL_PASSWORD"]
   }
 }
